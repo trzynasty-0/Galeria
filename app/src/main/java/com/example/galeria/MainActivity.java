7118b56411f09effe608e98a49d4b5bb3f0f23aa
trzynasty-0 <goniwiechamaja123@gmail.com>
2025-11-04 08:47:14 +0100

    zimportowanie obiektów do javy i utworzenie listy z obrazami

--- app/src/main/java/com/example/galeria/MainActivity.java
+++ app/src/main/java/com/example/galeria/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.galeria;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    private Button prev, next;
+    private ImageView zdjecie;
+    private EditText podanyNumer;
+    private Switch czyNiebieskie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +30,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        prev = findViewById(R.id.prevButton);
+        next = findViewById(R.id.nextButton);
+        zdjecie = findViewById(R.id.zdjecieView);
+        podanyNumer = findViewById(R.id.numerEdit);
+        czyNiebieskie = findViewById(R.id.zmienicKolorSwitch);
+
+        ArrayList<Integer> repozytorium = new ArrayList<>();
+        repozytorium.add(R.drawable.knight);
+        repozytorium.add(R.drawable.hornet);
+        repozytorium.add(R.drawable.together);
+
+
+
     }
 }
